e291aa15896527237de4b7ffca1210b984cc07f6
CodeByAnkita <ankitacode11@gmail.com>
2024-11-27 11:20:29 +0530

    added more things

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    # Fields for the Student model
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    image = models.ImageField()  # Optional: specify the upload directory
+    file = models.FileField()  # Optional: specify the upload directory
+
+    # def __str__(self):
+    #     return self.name  # Returns the name when the object is printed
+
+class Product(models.Model):
+    # Define fields for the Product model as needed
+    pass
--- home/views.py
+++ home/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
+
 def home(request):
-    return HttpResponse('Hello, World!')
+  peoples =[
+      {'name': 'John', 'age': 25},
+      {'name': 'Jane', 'age': 30},
+      {'name': 'Mark', 'age': 17},
+      {'name': 'Anna', 'age': 18}
+  ]
+  vegetables = [
+    'Carrot', 
+    'Tomato', 
+     'Cucumber', 
+    'Onion', 
+   'Broccoli', 
+  ]
+
+  return render (request, 'home/index.html', context={'page':'Django 2023 tutorial','peoples':peoples, 'vegetables':vegetables})
+def about(request):
+    context={'page': 'about'}
+    return render (request, 'home/about.html', context)
+
+def contact(request):
+    context={'page': 'contact'}
+    return render (request, 'home/contact.html', context)
+
+def success_page(request):
+ 
+    
+    return HttpResponse("<H1> Hey this is a Success! Page</H1>")
